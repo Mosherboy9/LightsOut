73b1620ef82814322c02a6227f993f66df064dd7
Jason De Beer <Jasondawiddebeer1@gmail.com>
2020-01-28 18:53:04 +0000

    Uploading Repository

---
+++ LightsOut.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace LightsOut
+{
+    partial class LightsOut
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NewGame = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.Exit = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.OnLights = new System.Windows.Forms.Label();
+            this.Restart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NewGame
+            // 
+            this.NewGame.Location = new System.Drawing.Point(12, 318);
+            this.NewGame.Name = "NewGame";
+            this.NewGame.Size = new System.Drawing.Size(75, 23);
+            this.NewGame.TabIndex = 0;
+            this.NewGame.Text = "New Game";
+            this.NewGame.UseVisualStyleBackColor = true;
+            this.NewGame.Click += new System.EventHandler(this.NewGame_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(178, 318);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 1;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(249, 234);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // OnLights
+            // 
+            this.OnLights.AutoSize = true;
+            this.OnLights.Location = new System.Drawing.Point(175, 280);
+            this.OnLights.Name = "OnLights";
+            this.OnLights.Size = new System.Drawing.Size(58, 13);
+            this.OnLights.TabIndex = 0;
+            this.OnLights.Text = "Lights On: ";
+            // 
+            // Restart
+            // 
+            this.Restart.Enabled = false;
+            this.Restart.Location = new System.Drawing.Point(93, 318);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(75, 23);
+            this.Restart.TabIndex = 2;
+            this.Restart.Text = "Restart";
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Blue = Light On\r\nWhite = Light Off";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Turn Off All The Lights!";
+            // 
+            // LightsOut
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 350);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.OnLights);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.NewGame);
+            this.MaximizeBox = false;
+            this.Name = "LightsOut";
+            this.Text = "Lights Out";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NewGame;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label OnLights;
+        private System.Windows.Forms.Button Restart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LightsOut.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightsOut
+{
+    public partial class LightsOut : Form
+    {
+        // Variables that are needed for the grid, 2d array, buttons, colors, and the count for the amount of lights that are turned on.
+        private LightsOutButton[,] LightButtons = new LightsOutButton[row,col];
+        private static int row = 5;
+        private static int col = 5;
+        Color[] lightColor = new Color[2] { Color.White, Color.Blue };
+        Random colors = new Random();
+        private int LightsOn = 0;
+        public LightsOut()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+        /// <summary>
+        /// Method that is used to instantiate the buttons and start the game.
+        /// </summary>
+        private void StartGame()
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    // Adding buttons to a 2d array.
+                    LightButtons[i, j] = new LightsOutButton
+                    {
+                        // Setting the size and color of the buttons.
+                        BackColor = lightColor[colors.Next(2)],
+                        Size = new Size(40, 40)
+                    };
+                    LightButtons[i, j].Enabled = true;
+                    // Adding on to the counter to indicate how many lights are turned on.
+                    if (LightButtons[i, j].BackColor == Color.Blue) LightsOn++;
+                    // setting the location for the buttons with spaces inbetween.
+                    LightButtons[i, j].Location = new Point(20 + (40 * i), 20 + (40 * j));
+                    LightButtons[i, j].xLoc = i;
+                    LightButtons[i, j].yLoc = j;
+                    LightButtons[i, j].TabIndex = i + j + 1;
+                    LightButtons[i, j].Click += Check_Lights;
+                    flowLayoutPanel1.Controls.Add(LightButtons[i, j]);
+                }
+            }
+            // Setting the text to show how many lights are on and refreshing it to be displayed.
+            OnLights.Text = "Lights On: " + LightsOn;
+            OnLights.Refresh();
+        }
+        /// <summary>
+        /// Method used to change the lights color.
+        /// </summary>
+        /// <param name="button"></param>
+        private void ChooseColor(LightsOutButton button)
+        {
+            if (button.BackColor == Color.Blue)
+            { 
+                button.BackColor = Color.White;
+            }
+            else
+            {
+                button.BackColor = Color.Blue;
+            }
+        }
+        /// <summary>
+        /// Method used to call the StartGame() Method and prevent certain buttons being clicked.
+        /// </summary>
+        private void NewGame_Click(object sender, EventArgs e)
+        {
+            StartGame();
+            NewGame.Enabled = false;
+            Restart.Enabled = true;
+        }
+        /// <summary>
+        /// Method used to swap the colors of the lights, and check to see how many lights are left to turn off.
+        /// </summary>
+        private void Check_Lights(object sender, EventArgs e)
+        {
+            LightsOutButton buttonclicked = (LightsOutButton)sender;
+            ChooseColor(buttonclicked);
+            int ButtonXloc = (buttonclicked.xLoc);
+            int ButtonYloc = (buttonclicked.yLoc);
+            //Checks and changes each button around the clicked button. (left side, right side, on top and below)
+            if (ButtonXloc > 0)
+            {
+                ChooseColor(LightButtons[ButtonXloc - 1, ButtonYloc]);
+            }
+            if (ButtonXloc < (row - 1))
+            {
+                ChooseColor(LightButtons[ButtonXloc + 1, ButtonYloc]);
+            }
+            if (ButtonYloc > 0)
+            {
+                ChooseColor(LightButtons[ButtonXloc, ButtonYloc - 1]);
+            }
+            if (ButtonYloc < (col - 1))
+            {
+                ChooseColor(LightButtons[ButtonXloc, ButtonYloc + 1]);
+            }
+            // Disables the buttons from being clicked when game is finished and displays all lights have been turned off.
+            if (GameFinished())
+            {
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < row; j++)
+                    {
+                        LightButtons[i, j].Enabled = false;
+                    }
+                }
+                MessageBox.Show("All Light Have Been Turned Off");
+                NewGame.Enabled = false;
+                Restart.Enabled = true;
+            }
+            // checks all the colors to give a new amount for the LightsOn counter.
+            LightsOn = 1;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    if (LightButtons[i, j].BackColor == Color.Blue) LightsOn++;
+                }
+            }
+            LightsOn--;
+            OnLights.Text = "Lights On: " + LightsOn;
+            OnLights.Refresh();
+        }
+        /// <summary>
+        /// Exits the application.
+        /// </summary>
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Boolean method to check if all the lights are turned off.
+        /// </summary>
+        /// <returns></returns>
+        private bool GameFinished()
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    if (LightButtons[i, j].BackColor == Color.Blue)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        /// <summary>
+        /// Method to restart the game and reset all the lights.
+        /// </summary>
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            LightsOn = 0;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    // Enables the buttons to be clicked again.
+                    LightButtons[i, j].Enabled = true;
+                    LightButtons[i, j].BackColor = lightColor[colors.Next(2)];
+                    if (LightButtons[i, j].BackColor == Color.Blue) LightsOn++;
+                    OnLights.Text = "Lights On: " + LightsOn;
+                    OnLights.Refresh();
+                }
+            }
+            
+        }
+    }
+}
